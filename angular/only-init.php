986ebc0bed169e2c7ad36b4d196b39120c2a83e2
tolla <tolla@bk.ru>
2016-02-03 23:42:02 +0200

    fix library angular

---
+++ angular/only-init.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anatoli
+ * Date: 03.02.2016
+ * Time: 23:29
+ */ ?><!doctype html>
+<html lang="en" ng-app>
+<head>
+    <meta charset="utf-8">
+    <title>My HTML File</title>
+    <link rel="stylesheet" href="/lib/css/bootstrap.css">
+    <script src="/lib/js/angular/1.0.6/angular.js"></script>
+</head>
+<body>
+
+<p>Nothing here {{'yet' + '!'}}</p>
+
+</body>
+</html>
